88220b81239598a6836de22942edbb7a19f3caeb
Prakash-Balu <prakash.engg89@gmail.com>
2025-04-04 19:13:24 +0530

    Environment url changes
    Environment url changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@
 export const environment = {
   production: true,
   razorPayKeyId: "rzp_test_UJT02PVMbPZRkF",
-  // baseURL: "https://veeras-api.onrender.com",
-  baseURL: "https://veerasapi-production.up.railway.app",
+  baseURL: "https://veeras-api.onrender.com",
+  // baseURL: "https://veerasapi-production.up.railway.app",
 };
