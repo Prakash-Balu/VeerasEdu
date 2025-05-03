42f6cdfd933038d45ab6e78a22a9ee6a1a6b0c37
Prakash-Balu <prakash.engg89@gmail.com>
2025-05-03 22:59:21 +0530

    Home page changes
    Home page changes

--- src/app/features/home/journey/journey.component.ts
+++ src/app/features/home/journey/journey.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-journey',
@@ -7,6 +7,38 @@ import { Component } from '@angular/core';
   templateUrl: './journey.component.html',
   styleUrl: './journey.component.css'
 })
-export class JourneyComponent {
+export class JourneyComponent implements AfterViewInit {
 
+  // Reference to the video element
+    @ViewChild('journey_video', { static: false }) videoElement!: ElementRef;
+  
+    // Flag to track if the user has scrolled
+    private hasScrolled = false;
+  
+    ngAfterViewInit(): void {
+      const video: HTMLVideoElement = this.videoElement.nativeElement;
+      if (video) {
+        video.muted = true;  // Ensure the video is muted initially
+      }
+    }
+  
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll(event: Event): void {
+      if (!this.hasScrolled) {
+        this.hasScrolled = true;  // Mark that the user has scrolled at least once
+        this.checkVideoInView();  // Check if the video is in the viewport after scrolling
+      }
+    }
+  
+    checkVideoInView(): void {
+      const video: HTMLVideoElement = this.videoElement.nativeElement;
+      const rect = video.getBoundingClientRect();
+  
+      // Check if the video is in the viewport
+      const isInView = rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+  
+      if (isInView && video.paused) {
+        video.play();  // Play the video if it's in the viewport
+      }
+    }
 }
